0d51fa6a9a4e81001c463726b392e88d75c1ac5c
gorayni <gorayni@gmail.com>
2017-02-06 18:50:23 +0100

    Confusion matrix visualization function added

--- __init__.py
+++ __init__.py
@@ -1,8 +1,12 @@
-from jinja2 import Environment, FileSystemLoader
-from IPython.display import display
-
 import inspect
 import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import sklearn.metrics as metrics
+from IPython.display import display
+from jinja2 import Environment, FileSystemLoader
 
 PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 TEMPLATE_ENVIRONMENT = Environment(autoescape=False, loader=FileSystemLoader(PATH), trim_blocks=False)
@@ -45,6 +49,34 @@ class Bold():
 
     def _repr_latex_(self):
         return self.latex
+
+
+def show_confusion_matrix(true_labels, predicted_labels, labels, figsize=None, normalize=True, annot=False, cmap='jet',
+                          label_size=10, linewidths=0):
+    if not figsize:
+        figsize = (5, 5)
+
+    cm = metrics.confusion_matrix(true_labels, predicted_labels)
+
+    # normalize confusion matrix
+    if normalize:
+        num_instances_per_class = cm.sum(axis=1)
+        cm = cm / num_instances_per_class[:, np.newaxis]
+
+    fig = plt.figure(figsize=figsize)
+    plt.clf()
+    ax = fig.add_subplot(111)
+    ax.set_aspect(1)
+
+    sns.heatmap(cm, annot=annot, cmap=cmap, linewidths=linewidths).get_figure()
+
+    num_labels = len(labels)
+
+    plt.yticks(range(num_labels), labels[::-1])
+    plt.xticks([], [])
+    for tick in ax.yaxis.get_major_ticks():
+        tick.label.set_fontsize(label_size)
+    return fig, ax
 
 
 def print_attribute(tag, value):
