c6d0a835a39ec531f89621a8265e133e85972bc3
gorayni <gorayni@gmail.com>
2017-02-08 18:01:57 +0100

    plot_datasets_summary function added

--- __init__.py
+++ __init__.py
@@ -2,14 +2,17 @@ from __future__ import division
 
 import inspect
 import os
+import types
 import warnings
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import sklearn.metrics as metrics
 from IPython.display import display
 from jinja2 import Environment, FileSystemLoader
+from pylab import annotate
 
 PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 TEMPLATE_ENVIRONMENT = Environment(autoescape=False, loader=FileSystemLoader(PATH), trim_blocks=False)
@@ -80,6 +83,35 @@ def show_confusion_matrix(true_labels, predicted_labels, labels, figsize=None, n
     plt.xticks([], [])
     for tick in ax.yaxis.get_major_ticks():
         tick.label.set_fontsize(label_size)
+    return fig, ax
+
+
+def plot_datasets_summary(stats, figsize=None, ylabel="Number of Instances", xlabel="Categories", annot_rotation=90,
+                          annot_fontsize=9, axis_fontsize=12):
+    if not figsize:
+        figsize = (1, 1)
+
+    sns.set_style("whitegrid", {'grid.color': '.9', 'axes.edgecolor': '.2', 'axes.linewidth': 1})
+    sns.set_context("notebook", font_scale=1, rc={"lines.linewidth": 1})
+
+    fig, ax = plt.subplots(1, 1, figsize=figsize, sharex=True)
+
+    stats.plot(kind='bar', ax=ax, color=[sns.color_palette("PuBu", 10)[-3]], legend=False)
+    plt.setp(ax.xaxis.get_majorticklabels(), rotation=annot_rotation, fontsize=axis_fontsize, ha='right')
+    plt.setp(ax.yaxis.get_majorticklabels(), fontsize=axis_fontsize)
+
+    images = stats['images'].tolist()
+    for w, k in zip(images, range(len(images))):
+        annotate("{:,d}".format(w), (k, w + 25), ha='center', fontsize=annot_fontsize)
+
+    SHIFT = -0.3  # Data coordinates
+    for label in ax.xaxis.get_majorticklabels():
+        label.customShiftValue = SHIFT
+        label.set_x = types.MethodType(lambda self, x: matplotlib.text.Text.set_x(self, x - self.customShiftValue),
+                                       label, matplotlib.text.Text)
+
+    plt.ylabel(ylabel, fontweight='bold', fontsize=12)
+    plt.xlabel(xlabel, fontweight='bold', fontsize=12)
     return fig, ax
 
 
